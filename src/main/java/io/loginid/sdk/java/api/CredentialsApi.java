032669719a52154a15049d110766f7d6ab30b22c
Mohammed Shamil <shamil@loginid.io>
2021-07-07 01:37:23 +0530

    Functions added in CredentialsApi
    Functions added to support api calls to /credentials/fido2/init/force

--- src/main/java/io/loginid/sdk/java/api/CredentialsApi.java
+++ src/main/java/io/loginid/sdk/java/api/CredentialsApi.java
@@ -667,4 +667,127 @@ public class CredentialsApi {
         apiClient.executeAsync(call, localVarReturnType, callback);
         return call;
     }
+
+    /**
+     * Build call for credentialsFido2InitForcePost
+     * @param body  (optional)
+     * @param requestID  (optional)
+     * @param progressListener Progress listener
+     * @param progressRequestListener Progress request listener
+     * @return Call to execute
+     * @throws ApiException If fail to serialize the request body object
+     */
+    public com.squareup.okhttp.Call credentialsFido2InitForcePostCall(CredentialsFido2InitForceBody body, UUID requestID, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener) throws ApiException {
+        Object localVarPostBody = body;
+
+        // create path and map variables
+        String localVarPath = "/credentials/fido2/init/force";
+
+        List<Pair> localVarQueryParams = new ArrayList<Pair>();
+        List<Pair> localVarCollectionQueryParams = new ArrayList<Pair>();
+
+        Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+        if (requestID != null)
+            localVarHeaderParams.put("Request-ID", apiClient.parameterToString(requestID));
+
+        Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+        final String[] localVarAccepts = {
+                "application/json"
+        };
+        final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+        if (localVarAccept != null) localVarHeaderParams.put("Accept", localVarAccept);
+
+        final String[] localVarContentTypes = {
+                "application/json"
+        };
+        final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+        localVarHeaderParams.put("Content-Type", localVarContentType);
+
+        if(progressListener != null) {
+            apiClient.getHttpClient().networkInterceptors().add(new com.squareup.okhttp.Interceptor() {
+                @Override
+                public com.squareup.okhttp.Response intercept(com.squareup.okhttp.Interceptor.Chain chain) throws IOException {
+                    com.squareup.okhttp.Response originalResponse = chain.proceed(chain.request());
+                    return originalResponse.newBuilder()
+                            .body(new ProgressResponseBody(originalResponse.body(), progressListener))
+                            .build();
+                }
+            });
+        }
+
+        String[] localVarAuthNames = new String[] {  };
+        return apiClient.buildCall(localVarPath, "POST", localVarQueryParams, localVarCollectionQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAuthNames, progressRequestListener);
+    }
+
+    @SuppressWarnings("rawtypes")
+    private com.squareup.okhttp.Call credentialsFido2InitForcePostValidateBeforeCall(CredentialsFido2InitForceBody body, UUID requestID, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener) throws ApiException {
+
+        com.squareup.okhttp.Call call = credentialsFido2InitForcePostCall(body, requestID, progressListener, progressRequestListener);
+        return call;
+
+    }
+
+    /**
+     * Begin adding FIDO2 authenticator as new credential (initiated by client backend)
+     * **Service token scope:** &#x60;credentials.force_add&#x60;
+     * @param body  (optional)
+     * @param requestID  (optional)
+     * @return InlineResponse20014
+     * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
+     */
+    public CredentialsFido2InitForceResponse credentialsFido2InitForcePost(CredentialsFido2InitForceBody body, UUID requestID) throws ApiException {
+        ApiResponse<CredentialsFido2InitForceResponse> resp = credentialsFido2InitForcePostWithHttpInfo(body, requestID);
+        return resp.getData();
+    }
+
+    /**
+     * Begin adding FIDO2 authenticator as new credential (initiated by client backend)
+     * **Service token scope:** &#x60;credentials.force_add&#x60;
+     * @param body  (optional)
+     * @param requestID  (optional)
+     * @return ApiResponse&lt;InlineResponse20014&gt;
+     * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
+     */
+    public ApiResponse<CredentialsFido2InitForceResponse> credentialsFido2InitForcePostWithHttpInfo(CredentialsFido2InitForceBody body, UUID requestID) throws ApiException {
+        com.squareup.okhttp.Call call = credentialsFido2InitForcePostValidateBeforeCall(body, requestID, null, null);
+        Type localVarReturnType = new TypeToken<CredentialsFido2InitForceResponse>(){}.getType();
+        return apiClient.execute(call, localVarReturnType);
+    }
+
+    /**
+     * Begin adding FIDO2 authenticator as new credential (initiated by client backend) (asynchronously)
+     * **Service token scope:** &#x60;credentials.force_add&#x60;
+     * @param body  (optional)
+     * @param requestID  (optional)
+     * @param callback The callback to be executed when the API call finishes
+     * @return The request call
+     * @throws ApiException If fail to process the API call, e.g. serializing the request body object
+     */
+    public com.squareup.okhttp.Call credentialsFido2InitForcePostAsync(CredentialsFido2InitForceBody body, UUID requestID, final ApiCallback<CredentialsFido2InitForceResponse> callback) throws ApiException {
+
+        ProgressResponseBody.ProgressListener progressListener = null;
+        ProgressRequestBody.ProgressRequestListener progressRequestListener = null;
+
+        if (callback != null) {
+            progressListener = new ProgressResponseBody.ProgressListener() {
+                @Override
+                public void update(long bytesRead, long contentLength, boolean done) {
+                    callback.onDownloadProgress(bytesRead, contentLength, done);
+                }
+            };
+
+            progressRequestListener = new ProgressRequestBody.ProgressRequestListener() {
+                @Override
+                public void onRequestProgress(long bytesWritten, long contentLength, boolean done) {
+                    callback.onUploadProgress(bytesWritten, contentLength, done);
+                }
+            };
+        }
+
+        com.squareup.okhttp.Call call = credentialsFido2InitForcePostValidateBeforeCall(body, requestID, progressListener, progressRequestListener);
+        Type localVarReturnType = new TypeToken<CredentialsFido2InitForceResponse>(){}.getType();
+        apiClient.executeAsync(call, localVarReturnType, callback);
+        return call;
+    }
 }
